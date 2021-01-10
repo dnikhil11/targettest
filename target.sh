5da84b7124c5b35c200b531e3e53644e6934daf4
dnikhil11 <77202081+dnikhil11@users.noreply.github.com>
2021-01-09 18:57:47 -0600

    Update target.sh

--- target.sh
+++ target.sh
@@ -1,8 +1,8 @@
-Number_of_lines = $(wc -l /home/nikhil/myapp.log | awk '{print $1}')
+Number_of_lines=$(wc -l myapp.log | awk '{print $1}')
 echo ${Number_of_lines}
 if [ $((Number_of_lines%2)) -eq 0 ]
 then
-  echo "Number is even."
+  echo Number is even
 else
-  echo "Number is odd."
+  echo Number is odd
 fi
