debd7a6cba711cdc60c0e9dfc9cd4ba48160f21e
dnikhil11 <77202081+dnikhil11@users.noreply.github.com>
2021-01-09 18:47:38 -0600

    Update target.sh

--- target.sh
+++ target.sh
@@ -1,6 +1,6 @@
-num_of_lines = $(wc -l /home/nikhil/app.log | awk'{print $1}')
-echo ${num_of_lines}
-if [ $((num_of_lines%2)) -eq 0 ]
+Number_of_lines = $(wc -l /home/nikhil/myapp.log | awk '{print $1}')
+echo ${Number_of_lines}
+if [ $((Number_of_lines%2)) -eq 0 ]
 then
   echo "Number is even."
 else
